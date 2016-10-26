23c949e4784399d19995e777b076f38f25e651f4
loveworldlovesky <zhouwenchao8426@126.com>
2016-10-26 16:45:39 +0800

    测试解决冲突:从eclipse提交并pushe

--- jk_study/jk/src/main/java/cn/stuty/jk/base/dao/BaseDao.java
+++ jk_study/jk/src/main/java/cn/stuty/jk/base/dao/BaseDao.java
@@ -14,4 +14,5 @@ public interface BaseDao<T> {
 	public void update(T entity);//修改，用实体作为参数
 	public void deleteById(Serializable id);//按id删除，删除一条；支持整数型和字符串类型ID
 	public void delete(Serializable[] ids);//批量删除；支持整数型和字符串类型ID
+	public void test();//测试解决冲突
 }
